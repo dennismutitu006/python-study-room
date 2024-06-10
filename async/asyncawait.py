de7744e2fb5fa24c66d45dfaa0ab8cfcf3d3806a
dennismutitu <dennismutitu06@gmail.com>
2024-06-10 14:50:25 +0300

    strict rules about async and await in python

--- async/asyncawait.py
+++ async/asyncawait.py
@@ -1,4 +1,15 @@
-async def g():
+async def g():#a func that u introduce with async def is a coroutine-func
+    '''
+    this function may use await, return,or yield but all are optional.
+    declaring async def noop(): pass is valis:
+    using await and/ or return creates a coroutine function. to call a such a func 
+    you must await it to get its results.
+    you can only use yield in an async def block only in python.This creates an asynchronouse generetor,
+    which you iterator over with async for.
+    anything defined with async def may not use yield from, which will raise a syntax error.
+    just like its a syntaxerr to use yield outside def func, it is a syntaxerr to use await 
+    outside of an async def coroutine.
+    '''
     # Pause here and come back to g() when f() is ready
     r = await f()
     return r
