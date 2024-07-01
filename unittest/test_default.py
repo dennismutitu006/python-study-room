671551cc6f388d9e53d722746d685458e36b3208
dennismutitu <dennismutitu06@gmail.com>
2024-07-01 05:00:46 +0300

    default test case using unittest

---
+++ unittest/test_default.py
@@ -0,0 +1,7 @@
+import unittest
+'''simplest TestCase subclass to implement a test method.'''
+
+class DefaultWidgetSizeTestCase(unittest.TestCase):
+    def test_default_widget_size(self):
+        widget = Widget('The widget')
+        self.assertEqual(widget.size(), (50, 50))
