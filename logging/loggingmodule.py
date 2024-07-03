619616991d97b08a227ed2c13b30f4963e3e4579
dennismutitu <dennismutitu06@gmail.com>
2024-07-03 10:18:13 +0300

    the python logging module

---
+++ logging/loggingmodule.py
@@ -0,0 +1,9 @@
+'''it provides a flexible framework for generating log messages from programs
+it allows you to:
+    set diff levels of logging(DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format log messages
+    direct log output to various destinations(console, files, email)
+    configure logging across multiple modules in ur app
+
+Understanding logging in this context is important for implementing robust data protection practices and ensuring compliance with data privacy regulations.
+'''
