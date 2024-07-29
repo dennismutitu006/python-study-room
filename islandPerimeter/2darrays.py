9a720717444f155f871e0a3f703e4d99b61da4ed
dennismutitu <dennismutitu06@gmail.com>
2024-07-29 12:30:14 +0300

    initializing 2d arrays method 1;

---
+++ islandPerimeter/2darrays.py
@@ -0,0 +1,10 @@
+#Python program to demonstrate working of method1 and method2
+rows, cols = (5, 5)
+#method 2 1st approach
+arr = [[0]*cols]*rows
+#lets change the 1st element of the 1st row to 1 and print the array
+arr[0][0] = 1
+
+for row in arr
+    print(row)
+
